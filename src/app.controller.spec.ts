743ac758b9e136410eeea429013fbabe3c5a783d
Артем  Мастренков <artemmastrenkov@MacBook-Pro-Artem.local>
2020-11-04 18:03:49 +0500

    Autoportal-2
    * Добавлены: details modules, controller
    Так же удалены hello-world файлы и строчки кода

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
+/*import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -20,3 +20,4 @@ describe('AppController', () => {
     });
   });
 });
+*/
---
+++ src/modules/details/services/details.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DetailsService {
+    getDetails() {
+        return [
+            {
+                name: "H4 Osram AllSeasons",
+                price: 800,
+                number: 4
+            }, 
+            {
+                name: "H1 Маяк",
+                price: 400,
+                number: 2
+            },
+            {
+                name: "Дворники всесезонные",
+                price: 800,
+                number: 4
+            }
+        ]
+    }
+}
